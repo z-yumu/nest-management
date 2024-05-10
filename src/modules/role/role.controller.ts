3dc65c8f273ced2e39d4854ae5bfd4d4aa967045
zhouyj <458675926@qq.com>
2024-05-10 17:13:02 +0800

    路由添加分组和去掉一些接口代码

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,16 +1,14 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
 } from '@nestjs/common'
 import { RoleService } from './role.service'
 import { CreateRoleDto } from './dto/create-role.dto'
 import { UpdateRoleDto } from './dto/update-role.dto'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('role')
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
@@ -20,23 +18,4 @@ export class RoleController {
     return this.roleService.create(createRoleDto)
   }
 
-  @Get()
-  findAll() {
-    return this.roleService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roleService.findOne(+id)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.roleService.update(+id, updateRoleDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roleService.remove(+id)
-  }
 }
